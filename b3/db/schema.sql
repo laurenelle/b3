23c88295c0a7e69a7b1c9bc824ed178573e5e36d
laurenelle <lauren.elle.t@gmail.com>
2017-04-10 12:58:02 -0700

    boop

---
+++ b3/db/schema.sql
@@ -0,0 +1,36 @@
+CREATE TABLE human (
+  id bigserial primary key,
+  name varchar(80),
+  daily_time int
+);
+
+CREATE TABLE dog (
+  id bigserial primary key,
+  name varchar(80),
+  age decimal,
+  weight decimal
+);
+
+CREATE TABLE dog_human (
+  id bigserial primary key,
+  dog_id integer references dog (id),
+  human_id integer references human (id)
+);
+
+CREATE TABLE training (
+  id bigserial primary key,
+  type varchar(80),
+  description text,
+  goals text
+);
+
+CREATE TABLE event (
+  id bigserial primary key,
+  duration int,
+  reward varchar(80),
+  trigger text,
+  dog_response text,
+  dog_id integer references dog (id),
+  human_id integer references human (id),
+
+);

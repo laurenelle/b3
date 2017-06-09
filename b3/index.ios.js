bedfe1af0199284aae0445fe2482b14a028f5e76
laurenelle <lauren.elle.t@gmail.com>
2017-06-09 13:12:40 -0700

    add dog component and clean up

--- b3/index.ios.js
+++ b3/index.ios.js
@@ -11,6 +11,15 @@ import {
   Text,
 } from 'react-native';
 
+class Dog extends Component {
+  render() {
+    return (
+      <Text>Hello {this.props.name}!</Text>
+    );
+  }
+}
+
+
 export default class b3 extends Component {
   render() {
     return (
@@ -18,118 +27,12 @@ export default class b3 extends Component {
         <Text style={styles.welcome}>
           B3!
         </Text>
+        <Dog name='Bane Buttercup' />
+        <Dog name='Bodhi Bear' />
+        <Dog name='Baloo Boo' />
       </View>
     );
   }
 }
-
-'use strict';
-
-// const React = require('react');
-const ReactNative = require('react-native');
-const {
-  Alert,
-  Button,
-  View,
-} = ReactNative;
-
-const onButtonPress = () => {
-  Alert.alert('Button has been pressed!');
-};
-
-exports.displayName = 'ButtonExample';
-exports.framework = 'React';
-exports.title = '<Button>';
-exports.description = 'Simple React Native button component.';
-
-exports.examples = [
-  {
-    title: 'Simple Button',
-    description: 'The title and onPress handler are required. It is ' +
-      'recommended to set accessibilityLabel to help make your app usable by ' +
-      'everyone.',
-    render: function() {
-      return (
-        <Button
-          onPress={onButtonPress}
-          title="Press Me"
-          accessibilityLabel="See an informative alert"
-        />
-      );
-    },
-  },
-  {
-    title: 'Adjusted color',
-    description: 'Adjusts the color in a way that looks standard on each ' +
-      'platform. On iOS, the color prop controls the color of the text. On ' +
-      'Android, the color adjusts the background color of the button.',
-    render: function() {
-      return (
-        <Button
-          onPress={onButtonPress}
-          title="Press Purple"
-          color="#841584"
-          accessibilityLabel="Learn more about purple"
-        />
-      );
-    },
-  },
-  {
-    title: 'Fit to text layout',
-    description: 'This layout strategy lets the title define the width of ' +
-      'the button',
-    render: function() {
-      return (
-        <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-          <Button
-            onPress={onButtonPress}
-            title="This looks great!"
-            accessibilityLabel="This sounds great!"
-          />
-          <Button
-            onPress={onButtonPress}
-            title="Ok!"
-            color="#841584"
-            accessibilityLabel="Ok, Great!"
-          />
-        </View>
-      );
-    },
-  },
-  {
-    title: 'Disabled Button',
-    description: 'All interactions for the component are disabled.',
-    render: function() {
-      return (
-        <Button
-          disabled
-          onPress={onButtonPress}
-          title="I Am Disabled"
-          accessibilityLabel="See an informative alert"
-        />
-      );
-    },
-  },
-];
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('b3', () => b3);
